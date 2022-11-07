12a3f6d2503e7dadaa45259d633567d046979117
Chris-STAT <chrisbai2015@gmail.com>
2022-11-07 10:33:32 -0600

    add files

--- homework4/homework/models.py
+++ homework4/homework/models.py
@@ -25,7 +25,7 @@ def extract_peak(heatmap, max_pool_ks=7, min_score=-5, max_det=100):
 
 
 class Detector(torch.nn.Module):
-
+    """
     class Res_Block(torch.nn.Module):
         def __init__(self, input_dim, output_dim, kernel_size=3, stride=2):
             super().__init__()
@@ -43,6 +43,21 @@ class Detector(torch.nn.Module):
 
         def forward(self,x):
             return F.relu(self.res_block(x) + self.skip(x))
+    """
+    class Res_block(torch.nn.Module):
+        def __init__(self, n_input, n_output, kernel_size=3, stride=2):
+            super().__init__()
+            self.c1 = torch.nn.Conv2d(n_input, n_output, kernel_size=kernel_size, padding=kernel_size // 2,
+                                      stride=stride, bias=False)
+            self.c2 = torch.nn.Conv2d(n_output, n_output, kernel_size=kernel_size, padding=kernel_size // 2, bias=False)
+            self.c3 = torch.nn.Conv2d(n_output, n_output, kernel_size=kernel_size, padding=kernel_size // 2, bias=False)
+            self.b1 = torch.nn.BatchNorm2d(n_output)
+            self.b2 = torch.nn.BatchNorm2d(n_output)
+            self.b3 = torch.nn.BatchNorm2d(n_output)
+            self.skip = torch.nn.Conv2d(n_input, n_output, kernel_size=1, stride=stride)
+
+        def forward(self, x):
+            return F.relu(self.b3(self.c3(F.relu(self.b2(self.c2(F.relu(self.b1(self.c1(x)))))))) + self.skip(x))
 
     class UpBlock(torch.nn.Module):
         def __init__(self, input_dim, output_dim, kernel_size=3, stride=2):
